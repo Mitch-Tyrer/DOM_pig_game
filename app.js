e48b690cc0f3dd9b449572a7a5e304591c23e2cb
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-08 13:28:09 -0700

    added newgame and functionality using a new init function

--- app.js
+++ app.js
@@ -8,31 +8,45 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer;
 
-var scores = [0,0],
+function init() {
+    scores = [0, 0],
     roundScore = 0,
     activePlayer = 0;
-    
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('#name-0').textContent = "Player 1";
+    document.querySelector('#name-1').textContent = "Player 1";
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+init();
+
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     //1.random number
-    var dice = Math.floor(Math.random()*6 + 1);
+    var dice = Math.floor(Math.random() * 6 + 1);
     //2. display the result
-    var diceDOM =  document.querySelector('.dice')
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
     //3. update the round scour IF the rolled number was NOT a 1
-    if(dice !== 1){
+    if (dice !== 1) {
         //Add to score
         roundScore += dice; //(roundScore = roundScore + dice);
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -57,19 +71,21 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
     )
 
-    : nextPlayer();
+        : nextPlayer();
     //Next Player
-    
+
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
-function nextPlayer () {
-    activePlayer === 0 ? activePlayer=1 : activePlayer=0;
-        roundScore = 0;
 
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
 }
