bc22b4f626eb186ad4fb61a902528915d9e1692c
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-08 10:40:50 -0700

    added initial variables and played with querySelector and css styles

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores = [0,0],
+    roundScore = 0,
+    activePlayer = 1,
+    dice = Math.floor(Math.random()*6 + 1);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.dice').style.display = 'none';
