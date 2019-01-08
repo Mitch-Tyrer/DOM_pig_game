04ddcd31cc9bc76f94a89801ae72921bc35ecf33
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-08 12:52:29 -0700

    added logic for rolling dice, calculating round scores, and switching the active player based on the roll of a 1

--- app.js
+++ app.js
@@ -11,10 +11,41 @@ GAME RULES:
 
 var scores = [0,0],
     roundScore = 0,
-    activePlayer = 1,
-    dice = Math.floor(Math.random()*6 + 1);
+    activePlayer = 0;
+    
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    //1.random number
+    var dice = Math.floor(Math.random()*6 + 1);
+    //2. display the result
+    var diceDOM =  document.querySelector('.dice')
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update the round scour IF the rolled number was NOT a 1
+    if(dice !== 1){
+        //Add to score
+        roundScore += dice; //(roundScore = roundScore + dice);
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer=1 : activePlayer=0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+});
